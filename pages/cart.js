d766846737e90154908f65ae3c31222dec72f98b
Tour <t.tour1995@gmail.com>
2021-02-16 17:21:32 +0700

    Update cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,4 +1,4 @@
-import {React, useState, Component} from 'react'
+import {React, useState, Component, useEffect} from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { Container, Row, Col, Card, Breadcrumb, Table, Form, Button } from 'react-bootstrap'
@@ -39,9 +39,8 @@ const Cart = () => {
                     let vat_list = (parseInt(itemToCheckout[key_item].price) * 7) / 100;
                     addOrderList({variables: {pid:itemToCheckout[key_item].id, qty: itemToCheckout[key_item].qty, price: itemToCheckout[key_item].price, order_id: data.addOrderShop.id, vat: vat_list}})
                 });
-
+                // console.log('test');
                 localStorage.removeItem("cart")
-                router.push('/')
             }else{
                 console.log('data retuen is null');
             }
@@ -56,25 +55,35 @@ const Cart = () => {
     });
 
 
-
-    const handleCHECKOUT = () => {
+    const handleCHECKOUT = async () => {
 
         // console.log(this.state.cart);
         const json = localStorage.getItem('cart')
-        const cart = JSON.parse(json)
-        // console.log(cart);
-        
-        
-        const vatToCheckout = cart.vat;
-        const subtotalToCheckout = cart.amount;
-        const totalToCheckout = subtotalToCheckout + vatToCheckout;
-        
-        
-        
-        addOrderShop({variables: {subtotal:subtotalToCheckout, vat: vatToCheckout, total: totalToCheckout}})
+        if (json != null) {
+            const cart = JSON.parse(json)
+            // console.log(cart);
+            
+            
+            const vatToCheckout = cart.vat;
+            const subtotalToCheckout = cart.amount;
+            const totalToCheckout = subtotalToCheckout + vatToCheckout;
+            
+            
+            
+            await addOrderShop({variables: {subtotal:subtotalToCheckout, vat: vatToCheckout, total: totalToCheckout}})
+            setCartQty(null)
+            router.push('/')
+        }else{
+            console.log('not items');
+        }
     }
+    useEffect(() => {
+        
+    })
+
 
     const listItems = useLocalStorage('cart', null);
+    // console.log(listItems);
     if (listItems != null) {
         var vatOrder = listItems.vat;
         var subtotalOrder = listItems.amount;
@@ -83,6 +92,50 @@ const Cart = () => {
         var subtotalOrder = 0;
     }
     const totalOrder = vatOrder + subtotalOrder;
+    // console.log(listItems);
+
+    const [cartQty, setCartQty] = useState(listItems ? listItems.items : null);
+    const [cartVat, setCartVat] = useState(listItems ? listItems.vat : null);
+    const [cartSubTotal, setCartSubTotal] = useState(listItems ? listItems.amount : null);
+    const [cartTotal, setCartTotal] = useState(listItems ? (listItems.vat + listItems.amount) : null);
+
+    const increaseQuantity = index => {
+        const currentItems = [...cartQty];
+        currentItems[index].qty += 1;
+        // localStorage.setItem('cart', JSON.stringify(currentItems));
+        setCartQty(currentItems);
+      };
+    
+      const decreaseQuantity = index => {
+        const currentItems = [...cartQty];
+    
+        if (currentItems[index].qty > 1) {
+          currentItems[index].qty -= 1;
+        //   localStorage.setItem('cart', JSON.stringify(currentItems));
+          setCartQty(currentItems);
+        }
+      };
+
+      useEffect(() => {
+        const json = localStorage.getItem('cart')
+        if (json != null) {
+            listItems.items = cartQty
+            var amount = 0
+            var vat = 0
+            Object.keys(listItems.items).forEach(function (key) {
+              amount = amount + (parseInt(listItems.items[key].price) * parseInt(listItems.items[key].qty));
+              vat = vat + (((parseInt(listItems.items[key].price) * parseInt(listItems.items[key].qty)) * 7) / 100) ;
+            });
+    
+            listItems.amount = amount
+            listItems.vat = vat
+            setCartVat(listItems.vat)
+            setCartSubTotal(listItems.amount)
+            setCartTotal((listItems.vat + listItems.amount))
+            localStorage.setItem('cart', JSON.stringify(listItems));
+        }
+       })
+
 
     return (
         <div>
@@ -121,13 +174,13 @@ const Cart = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                {listItems && listItems.items instanceof Array && listItems.items.map(prod => (
+                                {listItems && listItems.items instanceof Array && listItems.items.map((prod, i) => (
                                     <tr style={{textAlign:'center'}}>
                                         <td><img src={prod.img} style={{width: '50px'}} /></td>
                                         <td>{prod.name}</td>
                                         <td>
                                             <div style={{display: 'flex', justifyContent:'center'}}>
-                                                <button type="button" style={{border: "0 solid #e2e8f0"}} ><span >−</span></button>
+                                                <button type="button" style={{border: "0 solid #e2e8f0"}} onClick={() => decreaseQuantity(i)} ><span >−</span></button>
                                                 <Form.Control 
                                                     type="text" 
                                                     placeholder="Normal text" 
@@ -140,7 +193,7 @@ const Cart = () => {
                                                         border: "0 solid #e2e8f0"
                                                     }} 
                                                 />
-                                                <button type="button" style={{border: "0 solid #e2e8f0"}} ><span >+</span></button>
+                                                <button type="button" style={{border: "0 solid #e2e8f0"}} onClick={() => increaseQuantity(i)}><span >+</span></button>
                                             </div>
                                         </td>
                                         <td style={{textAlign:'right'}}>{prod.price * prod.qty}</td>
